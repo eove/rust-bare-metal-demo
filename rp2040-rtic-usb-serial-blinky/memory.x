7ebd7497e979df944c2fb13fb1e5bf522b26c3e1
Lionel Ains <lionel.ains@eove.fr>
2023-07-04 15:26:28 +0200

    Porting rtic example to nucleo-h723zg (but serial decoding commented-out for now)

--- rp2040-rtic-usb-serial-blinky/memory.x
+++ rp2040-rtic-usb-serial-blinky/memory.x
@@ -1,15 +1,13 @@
-MEMORY {
-    BOOT2 : ORIGIN = 0x10000000, LENGTH = 0x100
-    FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x100
-    RAM   : ORIGIN = 0x20000000, LENGTH = 256K
+MEMORY
+{
+  /* Flash memory bank 1 */
+  FLASH : ORIGIN = 0x08000000, LENGTH = 1024K
+
+  /* DTCM  */
+  RAM : ORIGIN = 0x20000000, LENGTH = 128K
 }
 
-EXTERN(BOOT2_FIRMWARE)
-
-SECTIONS {
-    /* ### Boot loader */
-    .boot2 ORIGIN(BOOT2) :
-    {
-        KEEP(*(.boot2));
-    } > BOOT2
-} INSERT BEFORE .text;
+/* This is where the call stack will be allocated. */
+/* The stack is of the full descending type. */
+/* NOTE Do NOT modify `_stack_start` unless you know what you are doing */
+_stack_start = ORIGIN(RAM) + LENGTH(RAM);
--- rp2040-rtic-usb-serial-blinky/src/main.rs
+++ rp2040-rtic-usb-serial-blinky/src/main.rs
@@ -8,7 +8,7 @@ use defmt_rtt as _;
 use panic_probe as _;
 
 defmt::timestamp! {"{=u64}", {
-    static COUNT: AtomicUsize = AtomicUsize::new(0);
+    static COUNT: AtomicUsize = AtomicUsize::new(0); // Transcript from ~40:30
     // NOTE(no-CAS) `timestamps` runs with interrupts disabled
     let n = COUNT.load(Ordering::Relaxed);
     COUNT.store(n + 1, Ordering::Relaxed);
@@ -16,94 +16,69 @@ defmt::timestamp! {"{=u64}", {
 }
 }
 
-#[rtic::app(device = rp_pico::hal::pac, peripherals = true, dispatchers = [XIP_IRQ])]
+#[rtic::app(device = stm32h7xx_hal::pac, peripherals = true, dispatchers = [USART1])]
 mod app {
     use core::mem::MaybeUninit;
     use defmt::*;
-    use embedded_hal::digital::v2::ToggleableOutputPin;
-    use rp_pico::{
-        hal::{
-            clocks::init_clocks_and_plls,
-            gpio::{pin::bank0::*, Pin, Pins, PushPullOutput},
-            usb::UsbBus,
-            watchdog::Watchdog,
-            Sio,
-        },
-        XOSC_CRYSTAL_FREQ,
+    
+    use stm32h7xx_hal::{
+        gpio::{self, gpiob::PB0, PushPull, Output, Pin},
+        delay::Delay,
+        prelude::*,
     };
-    use rtic_monotonics::rp2040::*;
-    use usb_device::{class_prelude::*, prelude::*};
-    use usbd_serial::SerialPort;
 
-    type LedPin = Pin<Gpio25, PushPullOutput>;
-
-    #[shared]
+    #[shared] // Multiple tasks can access this struct
     struct Shared {
         led_blink: bool,
         led_pause: u32,
-        msg_q: heapless::Deque<blink_proto::Message, 10>,
+        msg_q: heapless::Deque<blink_proto::Message, 10>, // double-ended queue
     }
 
-    #[local]
+    #[local] // Local info for the task that blinks the LED
     struct Local {
-        led: LedPin,
-        usb_dev: UsbDevice<'static, UsbBus>,
-        serial: SerialPort<'static, UsbBus>,
-        msg_buf: heapless::Vec<u8, { blink_proto::MSG_BUF_SIZE }>,
+        led: Pin<'B', 0, Output>,
+        //serial: SerialPort<'static, UsbBus>, //FIXME: adapt this for the USART on STM32
+        msg_buf: heapless::Vec<u8, { blink_proto::MSG_BUF_SIZE }>, // message buffer for the blink protocol with a static buffer size
+        delay: Delay
     }
 
-    #[init(local = [usb_bus: MaybeUninit<UsbBusAllocator<UsbBus>> = MaybeUninit::uninit()])]
-    fn init(c: init::Context) -> (Shared, Local) {
-        let mut resets = c.device.RESETS;
-        let monotoken = rtic_monotonics::create_rp2040_monotonic_token!();
-        Timer::start(c.device.TIMER, &mut resets, monotoken);
-        let mut watchdog = Watchdog::new(c.device.WATCHDOG);
-        let clocks = init_clocks_and_plls(
-            XOSC_CRYSTAL_FREQ,
-            c.device.XOSC,
-            c.device.CLOCKS,
-            c.device.PLL_SYS,
-            c.device.PLL_USB,
-            &mut resets,
-            &mut watchdog,
-        )
-        .ok()
-        .unwrap();
+    // #[monotonic(binds = SysTick, default = true)]
+    // type MyMono = DwtSystick<520_000_000>; // Our STM32H723 CPU is running at 520 MHz on (see the system clock setup below)
 
-        let sio = Sio::new(c.device.SIO);
-        let pins = Pins::new(
-            c.device.IO_BANK0,
-            c.device.PADS_BANK0,
-            sio.gpio_bank0,
-            &mut resets,
-        );
-        let led = pins.gpio25.into_push_pull_output();
+    #[init]
+    fn init(mut c: init::Context) -> (Shared, Local/*, init::Monotonics*/) {
+        let dp = c.device;
 
-        let usb_bus = c.local.usb_bus;
-        let usb_bus = usb_bus.write(UsbBusAllocator::new(UsbBus::new(
-            c.device.USBCTRL_REGS,
-            c.device.USBCTRL_DPRAM,
-            clocks.usb_clock,
-            true,
-            &mut resets,
-        )));
-        let serial = SerialPort::new(usb_bus);
+        c.core.DCB.enable_trace();
+        c.core.DWT.enable_cycle_counter();
 
-        let usb_dev = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27dd))
-            .manufacturer("Bedroom Builds")
-            .product("Serial port")
-            .serial_number("RTIC")
-            .device_class(usbd_serial::USB_CLASS_CDC)
-            .build();
+        // Constrain and Freeze power
+        let pwr = dp.PWR.constrain();
+        let pwrcfg = pwr.freeze();
 
-        let led_blink = true;
+        // Constrain and Freeze clock
+        let rcc = dp.RCC.constrain();
+        let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);    
+        
+        let delay = c.core.SYST.delay(ccdr.clocks);
 
-        led_blinker::spawn().ok();
+        let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+        let led = gpiob.pb0.into_push_pull_output();
+
+        /* FIXME: this should be adapted to use the USART on STM32H7 */
+        //let serial = SerialPort::new(usb_bus); // Create a serial port running over the USB device
+
+        let led_blink = true; // Set our variable => we want to blink
+
+        // Transcript from ~45:43
+
+        led_blinker::spawn().ok(); // Command to spawn the task that blinks the LED
 
         let msg_buf = heapless::Vec::<u8, { blink_proto::MSG_BUF_SIZE }>::new();
-        let msg_q = heapless::Deque::<blink_proto::Message, 10>::new();
+        let msg_q = heapless::Deque::<blink_proto::Message, 10>::new(); // We initialize our double-ended queue for messages
 
         info!("Send me a message!");
+        // Init function needs to return the information shared between the tasks
         (
             Shared {
                 led_blink,
@@ -112,112 +87,110 @@ mod app {
             },
             Local {
                 led,
-                usb_dev,
-                serial,
+                /*serial,*/
                 msg_buf,
+                delay,
             },
         )
     }
 
-    #[task(priority = 1, local = [led], shared = [led_blink, led_pause])]
+    #[task(priority = 1, local = [led, delay], shared = [led_blink, led_pause])] // We define the local data we want to access: led (note that only one task can have access to local members)
     async fn led_blinker(mut ctx: led_blinker::Context) {
         loop {
             let mut pause = 500;
-            (&mut ctx.shared.led_blink, &mut ctx.shared.led_pause).lock(|led_blink, led_pause| {
+            // Shared information must be locked. ctx is auto-populated from local and shared args in the task directive
+            (&mut ctx.shared.led_blink, &mut ctx.shared.led_pause).lock(|led_blink, led_pause| { // Closure will be run once the lock is grabbed
                 if *led_blink {
-                    ctx.local.led.toggle().ok();
+                    ctx.local.led.toggle();
                 }
                 pause = *led_pause;
             });
-            Timer::delay((pause as u64).millis()).await;
+            ctx.local.delay.delay_ms(pause as u16);
         }
     }
+    
+    /// USB interrupt handler. Runs every time the host requests new data.
+    // #[task(binds=USBCTRL_IRQ, local = [serial, usb_dev, msg_buf], shared = [led_blink, led_pause, msg_q])]
+    // fn on_usb(mut ctx: on_usb::Context) {
+    //     let serial = ctx.local.serial;
+    //     if !ctx.local.usb_dev.poll(&mut [serial]) {
+    //         return;
+    //     }
+    //     let mut buf = [0u8; blink_proto::MSG_BUF_SIZE];
+    //     let msg_buf = ctx.local.msg_buf;
+    //     match serial.read(&mut buf) {
+    //         Ok(count) if count > 0 => {
+    //             if msg_buf.extend_from_slice(&buf[..count]).is_err() {
+    //                 error!("bufcopy {}", count);
+    //                 msg_buf.clear();
+    //             }
+    //             use blink_proto::ParseResult::*;
+    //             match blink_proto::parse(msg_buf) {
+    //                 Found(msg) => {
+    //                     if let blink_proto::Message::Ping { id } = msg {
+    //                         info!("{:?}", msg);
+    //                         let pong = blink_proto::Message::Pong { id };
+    //                         ctx.shared.msg_q.lock(|q| q.push_back(pong).ok());
+    //                     }
+    //                     if let blink_proto::Message::Led {
+    //                         id,
+    //                         blinking,
+    //                         pause,
+    //                     } = msg
+    //                     {
+    //                         info!("{:?}", msg);
+    //                         ctx.shared.led_blink.lock(|b| *b = blinking);
+    //                         ctx.shared.led_pause.lock(|p| *p = pause);
+    //                         // confirm execution with the same message
+    //                         let pong = blink_proto::Message::Led {
+    //                             id,
+    //                             blinking,
+    //                             pause,
+    //                         };
+    //                         ctx.shared.msg_q.lock(|q| q.push_back(pong).ok());
+    //                     }
+    //                     msg_buf.clear();
+    //                     // Transcript for ~49:56
+    //                 }
+    //                 Need(b) => {
+    //                     debug!("Need({})", b);
+    //                 } // continue reading
+    //                 HeaderInvalid | DataInvalid => {
+    //                     debug!("invalid");
+    //                     msg_buf.clear();
+    //                 }
+    //             }
 
-    /// USB interrupt handler
-    #[task(binds=USBCTRL_IRQ, local = [serial, usb_dev, msg_buf], shared = [led_blink, led_pause, msg_q])]
-    fn on_usb(mut ctx: on_usb::Context) {
-        let serial = ctx.local.serial;
-        if !ctx.local.usb_dev.poll(&mut [serial]) {
-            return;
-        }
-        let mut buf = [0u8; blink_proto::MSG_BUF_SIZE];
-        let msg_buf = ctx.local.msg_buf;
-        match serial.read(&mut buf) {
-            Ok(count) if count > 0 => {
-                if msg_buf.extend_from_slice(&buf[..count]).is_err() {
-                    error!("bufcopy {}", count);
-                    msg_buf.clear();
-                }
-                use blink_proto::ParseResult::*;
-                match blink_proto::parse(msg_buf) {
-                    Found(msg) => {
-                        if let blink_proto::Message::Ping { id } = msg {
-                            info!("{:?}", msg);
-                            let pong = blink_proto::Message::Pong { id };
-                            ctx.shared.msg_q.lock(|q| q.push_back(pong).ok());
-                        }
-                        if let blink_proto::Message::Led {
-                            id,
-                            blinking,
-                            pause,
-                        } = msg
-                        {
-                            info!("{:?}", msg);
-                            ctx.shared.led_blink.lock(|b| *b = blinking);
-                            ctx.shared.led_pause.lock(|p| *p = pause);
-                            // confirm execution with the same message
-                            let pong = blink_proto::Message::Led {
-                                id,
-                                blinking,
-                                pause,
-                            };
-                            ctx.shared.msg_q.lock(|q| q.push_back(pong).ok());
-                        }
-                        msg_buf.clear();
-                    }
-                    Need(b) => {
-                        debug!("Need({})", b);
-                    } // continue reading
-                    HeaderInvalid | DataInvalid => {
-                        debug!("invalid");
-                        msg_buf.clear();
-                    }
-                }
-
-                // write back to the host
-                loop {
-                    let msg = ctx.shared.msg_q.lock(|q| q.pop_front());
-                    let bytes = match msg {
-                        Some(msg) => match blink_proto::wrap_msg(msg) {
-                            Ok(msg_bytes) => msg_bytes,
-                            Err(_) => break,
-                        },
-                        None => break,
-                    };
-                    let mut wr_ptr = bytes.as_slice();
-                    while !wr_ptr.is_empty() {
-                        let _ = serial.write(wr_ptr).map(|len| {
-                            wr_ptr = &wr_ptr[len..];
-                        });
-                    }
-                }
-            }
-            _ => {}
-        }
-    }
+    //             // write back to the host
+    //             loop {
+    //                 let msg = ctx.shared.msg_q.lock(|q| q.pop_front());
+    //                 let bytes = match msg {
+    //                     Some(msg) => match blink_proto::wrap_msg(msg) {
+    //                         Ok(msg_bytes) => msg_bytes,
+    //                         Err(_) => break,
+    //                     },
+    //                     None => break,
+    //                 };
+    //                 let mut wr_ptr = bytes.as_slice();
+    //                 while !wr_ptr.is_empty() {
+    //                     let _ = serial.write(wr_ptr).map(|len| {
+    //                         wr_ptr = &wr_ptr[len..];
+    //                     });
+    //                 }
+    //             }
+    //         }
+    //         _ => {}
+    //     }
+    // }
 
     /// Task with least priority that only runs when nothing else is running.
-    /// avoids rtic sending the board to sleep
-    /*
-    #[idle(local = [x: u32 = 0])]
-    fn idle(_cx: idle::Context) -> ! {
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        /* Could also be (to avoid rtic sending the board to sleep):
         loop {
             cortex_m::asm::nop();
         }
-    }
-    */
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
+         */
         // debug::exit(debug::EXIT_SUCCESS);
         loop {
             // hprintln!("idle");
